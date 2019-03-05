1d14bcc0f117ca4feaf4c95138a57c71122b94aa
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-05 17:00:37 -0600

    Implement API call to openweathermap.

---
+++ WeatherJS/app.js
@@ -0,0 +1,12 @@
+const weather = new Weather('Dallas', 'Texas');
+
+// Get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather());
+
+function getWeather() {
+	weather.getWeather()
+		.then(results => {
+			console.log(results)
+		})
+		.catch(err => console.log(err))
+}
---
+++ WeatherJS/weather.js
@@ -0,0 +1,22 @@
+class Weather {
+	constructor(city, state) {
+		this.api_key = '29a2da783f3475bc49ac56bc288e0f95';
+		this.city = city;
+		this.state = state;
+	}
+
+	// FETCH from api
+	async getWeather() {
+		const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.state}&appid=${this.api_key}`);
+
+		const responseData = await response.json();
+
+		return responseData;
+	}
+
+	// Change the weather location
+	changeLocation(city, state) {
+		this.city = city;
+		this.state = state;
+	}
+}

c99e15f1928e05303a8717d3428a2c6186396b8c
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-07 15:46:39 -0600

    Demonstrate basics of Observer pattern in JS, using ES5 prototypes and ES6 classes.

--- Design-Patterns-Intro/es6.js
+++ Design-Patterns-Intro/es6.js
@@ -1,16 +1,19 @@
 // subscribe/unsubscribe to different events that happen during the lifetime of your app
 
-function EventObserver() {
-	//functions passed into into EventObserver
-	this.observers = [];
-}
+// ES6 version
 
-EventObserver.prototype = {
-	subscribe: function(fn) {
+class EventObserver {
+	constructor() {
+		//functions passed into into EventObserver
+		this.observers = [];
+	}
+
+	subscribe(fn) {
 		this.observers.push(fn);
 		console.log(`You are now subscribed to ${fn.name}`);
-	},
-	unsubscribe: function(fn) {
+	}
+
+	unsubscribe(fn) {
 		this.observers = this.observers.filter(function(item){
 			if(item !== fn) {
 				//return an array containing everything EXCEPT the unsubscribe, assign that to observers array, thereby updating it
@@ -18,8 +21,9 @@ EventObserver.prototype = {
 			}
 		});
 		console.log(`You are now unsubscribed from ${fn.name}`);
-	},
-	fire: function() {
+	}
+
+	fire() {
 		this.observers.forEach(function(obs) {
 			//call() calls the function with a given this value and individually provided arguments, though we're not passing those in right now.
 			obs.call();
